f8e45a49533eace973034d3638ea67135f6e9f14
JoaoGripp <corguinha@gmail.com>
2022-07-26 09:41:19 -0300

    add animation on orderButton

--- OrderApp/MenuItemDetailViewController.swift
+++ OrderApp/MenuItemDetailViewController.swift
@@ -38,4 +38,10 @@ class MenuItemDetailViewController: UIViewController {
         detailTextLabel.text = menuItem.detailText
     }
 
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: [], animations: {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: nil)
+    }
 }
