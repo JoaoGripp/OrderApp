5dac11c00f3214c7605dccaddb9ee8b98a0dc12a
JoaoGripp <corguinha@gmail.com>
2022-07-26 11:13:30 -0300

    added edit functionality to orderItem list

--- OrderApp/OrderTableViewController.swift
+++ OrderApp/OrderTableViewController.swift
@@ -12,6 +12,8 @@ class OrderTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         NotificationCenter.default.addObserver(tableView!, selector: #selector(UITableView.reloadData), name: MenuController.orderUpatedNotification, object: nil)
         
     }
@@ -42,6 +44,16 @@ class OrderTableViewController: UITableViewController {
         content.text = menuItem.name
         content.secondaryText = menuItem.price.formatted(.currency(code: "usd"))
         cell.contentConfiguration = content
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            MenuController.shared.order.menuItems.remove(at: indexPath.row)
+        }
     }
 
     
