b6e3e6a6734c34b8b2e0a0bb88eb6631c4a978c8
JoaoGripp <corguinha@gmail.com>
2022-07-26 13:53:07 -0300

    Add placeholder image on the cells

--- OrderApp/MenuTableViewController.swift
+++ OrderApp/MenuTableViewController.swift
@@ -74,6 +74,7 @@ class MenuTableViewController: UITableViewController {
         var content = cell.defaultContentConfiguration()
         content.text = menuItem.name
         content.secondaryText = menuItem.price.formatted(.currency(code: "usd"))
+        content.image = UIImage(systemName: "photo.on.rectangle")
         cell.contentConfiguration = content
     }
     
