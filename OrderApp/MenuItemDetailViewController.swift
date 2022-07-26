57a8159cf734628d5e7eb221ec1f8d3d18958a20
JoaoGripp <corguinha@gmail.com>
2022-07-26 09:34:47 -0300

    Updated labels on MenuDetail

--- OrderApp/MenuItemDetailViewController.swift
+++ OrderApp/MenuItemDetailViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     let menuItem: MenuItem
     
     init?(coder: NSCoder, menuItem: MenuItem) {
@@ -23,18 +29,13 @@ class MenuItemDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateUI()
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
+        detailTextLabel.text = menuItem.detailText
     }
-    */
 
 }
